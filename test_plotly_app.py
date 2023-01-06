e918680fc2fa7a227abd2024e5f755eb3e6330fd
cjpaine <christopher.paine@hotmail.co.uk>
2023-01-06 23:07:29 +0000

    initial test using excel spreadsheets to hold data

--- test_plotly_app.py
+++ test_plotly_app.py
@@ -2,27 +2,13 @@
 import mysql.connector
 import pandas as pd
 
-# Connect to the MySQL server
-#cnx = mysql.connector.connect(user='root', password='ActuarialDashboard', host='ELITEBOOK', database='mydb')
-cnx = mysql.connector.connect(user='Christopher Paine', password='ActuarialDashboard', host='ELITEBOOK', database='mydb')
-print(cnx)
-
-# Create a cursor object to execute SQL statements
-cursor = cnx.cursor()
-
-# Execute a SQL SELECT statement to retrieve data from the database
-query = "SELECT * FROM AMC00"
-#cursor.execute(query)
-
-# populate dataframe by sql
-df = pd.read_sql(query,con=cnx)
+# Load the data
+df = pd.read_excel('Mortality_tables/ams00.xls')
 
 #print what has been retrieved
 print(df.head)
 
-# Close the cursor and connection when you are finished
-cursor.close()
-cnx.close()
+
 
 
 #-------------------------------------------------------------
@@ -35,7 +21,7 @@ import plotly.express as px
 app = dash.Dash()
 
 # Create a plotly figure
-fig = px.line(df, x='Age', y='q_[x]')
+fig = px.line(df, x='Age x', y='Duration 0')
 
 # Create a Dash layout
 app.layout = html.Div([
