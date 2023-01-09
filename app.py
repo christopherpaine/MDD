7cafca40bd204450a3f8a2c3ead8a8bcf2aa19cd
cjpaine <christopher.paine@hotmail.co.uk>
2023-01-09 03:23:35 +0000

    first version to be sent out for review

--- app.py
+++ app.py
@@ -87,34 +87,13 @@ trace_3 = go.Scatter(x=df_dset_3['Age x'], y=df_dset_3['Duration 0'])
 
 fig = go.Figure(data=[trace_1, trace_2, trace_3])
 
-
-
-
-
-
 #-------------------------------------------------------------------------------------
-
+#                SORT THE APP AND THE SERVER
 #--------------------------------------------------------------------------
 
 # Initialize the app
-#app = dash.Dash()
-
-
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 server = app.server
-
-
-
-#--------------------------------------------------------------------------------------------------------------
-#                               SOME FUNCTIONS
-#----------------------------------------------------------------------------------------------------------------
-
-
-
-
-
-
-
 
 #-------------------------------------------------------------------------------------
 #           CREATE SOME CARD OBJECTS
@@ -227,10 +206,12 @@ output_card = dbc.Card(
             [
                 
                 dcc.Graph(figure=fig,id='graph'),
-                dbc.Label("Truncate Axis"),
+                dbc.Label("Truncate X-Axis"),
                 dcc.RangeSlider(0,120,10,value=[0,120],
-                    id='graph_slider',allowCross=False,pushable=20
-              
+                    id='graph_slider',allowCross=False,pushable=20),
+                dbc.Label("Truncate Y-Axis"),
+                dcc.RangeSlider(0,1,0.1,value=[0,1],
+                    id='graph_slider2',allowCross=False,pushable=0.1
                 )
             ]
         )
@@ -316,9 +297,10 @@ def update_table3_options_from_dsource(dsource,descrip):
      Input(component_id='select_slider_1', component_property='value'),
      Input(component_id='select_slider_2', component_property='value'),
      Input(component_id='select_slider_3', component_property='value'),
-     Input(component_id='graph_slider', component_property='value')]
+     Input(component_id='graph_slider', component_property='value'),
+     Input(component_id='graph_slider2', component_property='value')]
 )
-def update_figure(sheet_name1, sheet_name2, sheet_name3, chart_type,slider_1,slider_2,slider_3,graph_slider_value):
+def update_figure(sheet_name1, sheet_name2, sheet_name3, chart_type,slider_1,slider_2,slider_3,graph_slider_value,graph_slider_value2):
     df_dset_1 = pd.read_excel('Mortality_tables/00series.xls', sheet_name=sheet_name1)
     df_dset_2 = pd.read_excel('Mortality_tables/00series.xls', sheet_name=sheet_name2)
     df_dset_3 = pd.read_excel('Mortality_tables/00series.xls', sheet_name=sheet_name3)
@@ -354,26 +336,46 @@ def update_figure(sheet_name1, sheet_name2, sheet_name3, chart_type,slider_1,sli
     print("graph slider value")
     print(graph_slider_value)
 
+    
+
     if sheet_name1 is not None:
         if chart_type == 'line':
             trace_1 = go.Scatter(x=df_dset_1['Age x'], y=df_dset_1[duration_dset_1], name=sheet_name1, marker=dict(color="#abe2fb"))
         elif chart_type == 'bar':
             trace_1 = go.Bar(x=df_dset_1['Age x'], y=df_dset_1[duration_dset_1 ], name=sheet_name1, marker=dict(color="#abe2fb"))
         data.append(trace_1)
+
+
     if sheet_name2 is not None:
         if chart_type == 'line':
             trace_2 = go.Scatter(x=df_dset_2['Age x'], y=df_dset_2[duration_dset_2], name=sheet_name2,marker=dict(color="#002c53"))
         elif chart_type == 'bar':
             trace_2 = go.Bar(x=df_dset_2['Age x'], y=df_dset_2[duration_dset_2], name=sheet_name2,marker=dict(color="#002c53"))
         data.append(trace_2)
+
     if sheet_name3 is not None:
         if chart_type == 'line':
             trace_3 = go.Scatter(x=df_dset_3['Age x'], y=df_dset_3[duration_dset_3], name=sheet_name3,marker=dict(color=" #c3941e"))
         elif chart_type == 'bar':
             trace_3 = go.Bar(x=df_dset_3['Age x'], y=df_dset_3[duration_dset_3], name=sheet_name3,marker=dict(color=" #c3941e"))
         data.append(trace_3)
+
+
     
+
     fig = go.Figure(data=data)
+
+    y_max_list =[]
+    # iterate through the traces
+    for trace in fig.data:
+        # find the maximum y value for the current trace
+        y_max = max(trace.y)
+        # append the maximum y value to the list
+        y_max_list.append(y_max)
+
+
+
+
     # Set the xaxis title to "Age"
     fig.update_layout(xaxis=dict(title="Ageₓ"))
     # Set the yaxis title to "qₓ"
@@ -381,9 +383,10 @@ def update_figure(sheet_name1, sheet_name2, sheet_name3, chart_type,slider_1,sli
     fig.update_layout(
                     paper_bgcolor='white',
                     plot_bgcolor='white',
-                    xaxis=dict(gridcolor='white',range=graph_slider_value),
-                    yaxis=dict(gridcolor='white')
-                    )
+                    xaxis=dict(gridcolor='white',range=graph_slider_value))
+    
+    fig.update_layout(yaxis=dict(gridcolor='white', range=graph_slider_value2))
+                    
 
     return fig,max_select_dset_1,max_select_dset_2,max_select_dset_3
 
